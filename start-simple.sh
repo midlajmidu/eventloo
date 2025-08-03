985b77cfbab5a807cd5d833559809a75d54b775c
muhammed midlaj <muhammedmidlaj@MJs-MacBook-Air.local>
2025-08-03 10:09:48 +0530

    🧹 Remove all .sh files - cleanup deployment scripts

--- start-simple.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting Django application..."
-
-# Run migrations
-python manage.py migrate --noinput
-
-# Start Gunicorn
-exec gunicorn --bind 0.0.0.0:$PORT --workers 1 --timeout 120 event_management.wsgi:application 
